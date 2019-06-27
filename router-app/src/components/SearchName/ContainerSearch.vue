e5223cc43ccfe02289da40f62893d2207ab260c7
Roberto Marçal <cmarcal.roberto@gmail.com>
2019-06-26 23:52:39 -0300

    Roberto
    No filtro por nome, agora aparece o resultado com o layout igual da lista de herois

--- router-app/src/components/SearchName/ContainerSearch.vue
+++ router-app/src/components/SearchName/ContainerSearch.vue
@@ -1,37 +1,56 @@
 <template>
   <div class="hello">
-    <h2>{{msg}}</h2>
-    <input v-model="valeuSearch" placeholder="ex.: iron man">
-    <p>{{searchHero}}</p>
-    <p>{{info2}}</p>
+    <h2>Find one hero</h2>
+    <div class="inputSearch">
+      <input v-model="valeuSearch" placeholder="ex.: iron man">
+      <i class="fas fa-search"></i>
+    </div>
+    <CardHero v-if="!isLoading" :heros="heros" @heroClick="onHeroClick"/>
+    <ModalHero v-if="isModalShowing" :idHero="idHero" :dataHero="dataHero" @closeModal="onCloseModal"/>
   </div>
 </template>
 
 <script>
 import {HTTP} from '@/Api/api'
+import CardHero from '../../_common/CardHero'
+import ModalHero from '../../_common/ModalHero'
 
 export default {
   name: 'ContainerSearch',
+  components: {CardHero, ModalHero},
   data () {
     return {
-      msg: 'Escolha um Heroi',
-      info: null,
-      info2: null,
-      valeuSearch: ''
+      heros: null,
+      valeuSearch: '',
+      isModalShowing: false,
+      isLoading: true,
+      idHero: null,
+      dataHero: null
+    }
+  },
+  methods: {
+    onHeroClick (obj) {
+      this.isModalShowing = true
+      this.idHero = obj.id
+      this.dataHero = obj
+    },
+    onCloseModal () {
+      this.isModalShowing = false
     }
   },
   computed: {
     searchHero: function () {
       HTTP.get(process.env.TOOKEN + '/search/' + this.valeuSearch)
         .then(response => {
-          this.info = response.data.results
+          this.isLoading = false
+          this.heros = response.data
         })
         .catch(err => {
           console.log(err.message)
         })
 
       if (this.valeuSearch !== '') {
-        return this.info
+        return this.heros
       }
     }
   }
@@ -39,19 +58,29 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
 h1, h2 {
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.inputSearch {
+  input {
+    margin: 20px 0;
+    width: 40%;
+    border: none;
+    border-bottom: 2px solid #2a3542;
+
+    &::placeholder {
+      padding: 5px;
+    }
+    &:focus{
+    outline: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  }
+  i {
+    position: relative;
+    right: 30px;
+    color: #2a3542
+  }
 }
 </style>
